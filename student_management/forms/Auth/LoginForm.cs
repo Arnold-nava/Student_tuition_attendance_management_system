c8428bf6b40602ac055086be75e11aed7f67da0c
Arnold nava <arnoldnava995@gmail.com>
2026-04-25 23:53:36 +0800

    first commit

---
+++ student_management/forms/Auth/LoginForm.cs
@@ -0,0 +1,10 @@
+namespace student_management.forms.Auth
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ student_management/helpers/Session.cs
@@ -0,0 +1,11 @@
+﻿namespace crud.Helpers
+{
+    public class Session
+    {
+        public static int userId;
+        public static int studentId;
+        public static string username;
+        public static string role;
+        public static string fullName;
+    }
+}
